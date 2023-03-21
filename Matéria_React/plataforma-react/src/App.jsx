6615e6ce1bead4307ad462d5a8258136edd3cc6b
leopachecof <l.nikiti@gmail.com>
2023-03-21 13:13:27 -0300

    ajuste

---
+++ Matéria_React/plataforma-react/src/App.jsx
@@ -0,0 +1,25 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Menu } from './components/Menu/Menu';
+import { Home } from './pages/Home/Home';
+import { Blog } from './pages/Blog/Blog';
+import { Contato } from './pages/Contato/Contato';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/contato' element={<Contato />} />
+        <Routes/>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Matéria_React/plataforma-react/src/components/Menu/Menu.jsx
@@ -0,0 +1,17 @@
+export function Menu() {
+    return (
+        <div className="menu">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
