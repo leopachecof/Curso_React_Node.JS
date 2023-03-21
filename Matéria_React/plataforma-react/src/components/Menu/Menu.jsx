753d2ae3f72d55bd53cef9390f5e1a9e60bc2dbc
leopachecof <l.nikiti@gmail.com>
2023-03-21 12:09:42 -0300

    corrigindo local de repositório

---
+++ Matéria_React/plataforma-react/src/components/Menu/Menu.jsx
@@ -0,0 +1,17 @@
+export function Menu() {
+    return (
+        <div className="menu">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+                <li>
+                    <a href="#">Contato</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
